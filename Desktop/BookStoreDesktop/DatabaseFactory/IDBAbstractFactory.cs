a2798f4c85df9547ad9de1b6cfac7c801826032e
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-04-02 15:40:12 +0700

    Database Provider Factory

---
+++ Desktop/BookStoreDesktop/DatabaseFactory/IDBAbstractFactory.cs
@@ -0,0 +1,14 @@
+﻿using LibraryAbstractDBProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreDesktop.DatabaseFactory
+{
+    public interface IDBAbstractFactory
+    {
+        IDatabaseProvider GetDatabase();
+    }
+}
