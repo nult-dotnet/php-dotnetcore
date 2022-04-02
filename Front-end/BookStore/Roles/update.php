84303b74362830e46ccc1716a71febe71d5a9d79
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-04-02 14:32:51 +0700

    CURL Role

---
+++ Front-end/BookStore/Roles/update.php
@@ -0,0 +1,16 @@
+<?php
+    include __DIR__ . '/../CallAPI.php';
+    if(isset($_POST["Update"])){
+        $request = "role/".$_POST["Id"];
+        $data = array(
+            "Name" => $_POST["Name"]
+        );
+        $headers = array("Content-Type: application/json");
+        $result = CallAPI($request,"PUT",json_encode($data),$headers);
+        if(isset($result["Error"]) || isset($result["errors"])){
+            print_r($result);
+        }else{
+            echo '<script>location.href = "index.php";</script>';
+        }
+    }
+?>
