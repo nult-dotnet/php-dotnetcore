195faaded9ccb3e9c8d7e50098587f8a8af6eb1d
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-04-02 14:09:07 +0700

    Config SQLServer

---
+++ Back-end/LibraryAbstractDBProvider/LibraryAbstractDBProvider/DBProvider/SQLServer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryAbstractDBProvider.DBProvider
+{
+    public class SQLServer : AbstractDBProvider
+    {
+        public override void ConnectedDatabase(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString(), x => x.MigrationsAssembly("SqlServerMigrations"));
+        }
+        public override string ConnectionString()
+        {
+            IConfiguration config = GetStringAppsetting.ConnectString();
+            return config.GetConnectionString("SQLServer");
+        }
+    }
+}
