d02e23398defd7f38a6a45acb17edbc3c65f0e4e
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-04-02 14:25:20 +0700

    Service interface

---
+++ Desktop/BookStoreDesktop/Interfaces/Services/IRoleService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStoreDesktop.Models;
+namespace BookStoreDesktop.Interfaces.Services
+{
+    public interface IRoleService
+    {
+        List<Role> GetAllRole();
+        List<Role> GetRoleByName(string name);
+        bool CreateRole(RoleDTO roleDTO);
+        bool UpdateRole(RoleDTO roleDTO,int id);
+        bool DeleteRole(int id);
+    }
+}
