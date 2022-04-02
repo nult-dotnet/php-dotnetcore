650f02a3a33ef63539f7ccf7464a4ef3b13a2280
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-04-02 14:03:22 +0700

    Services

---
+++ Back-end/BookStoreApi/Services/BillsService.cs
@@ -0,0 +1,20 @@
+﻿using BookStoreApi.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using BookStoreApi.RepositoryPattern;
+using BookStoreApi.Interfaces;
+namespace BookStoreApi.Services
+{
+    public class BillsService : IBillService
+    {
+        private readonly IUnitOfWork<Bill> _unitOfWork;
+        public BillsService()
+        {
+            this._unitOfWork = GetUnitOfWork<Bill>.UnitOfWork();
+        }
+        public async Task<IEnumerable<Bill>> GetBills() => await this._unitOfWork.Repository.Get();
+        public async Task<Bill?> GetBillById(string id) => await this._unitOfWork.Repository.GetByID(id);
+        public async Task CreateBill(Bill newBill) => await this._unitOfWork.Repository.Insert(newBill);
+        public async Task UpdateBill(string id, Bill updateBill) => await this._unitOfWork.Repository.Update(updateBill);
+    }
+}
---
+++ Back-end/BookStoreApi/Services/CategoryService.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+using BookStoreApi.Models;
+using Microsoft.Extensions.Options;
+using BookStoreApi.RepositoryPattern;
+using BookStoreApi.Interfaces;
+namespace BookStoreApi.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IUnitOfWork<Category> _unitOfWork;
+        public CategoryService()
+        {
+            this._unitOfWork = GetUnitOfWork<Category>.UnitOfWork();
+        }
+        public async Task<IEnumerable<Category>> GetCategory() => await this._unitOfWork.Repository.Get();
+        public async Task<Category?> GetCategoryById(string id) => await this._unitOfWork.Repository.GetByID(id);
+        public async Task CreateCategory(Category newCategory) => await this._unitOfWork.Repository.Insert(newCategory);
+        public async Task UpdateCategory(string id,Category updateCategory) => await this._unitOfWork.Repository.Update(updateCategory);
+        public async Task DeleteCategory(string id) => await this._unitOfWork.Repository.Delete(id);
+        public async Task<IEnumerable<Category>> ValidateCategory(string id, string name) => await this._unitOfWork.Repository.Get(x=>x.Id != id && x.Name == name);
+    }
+}
