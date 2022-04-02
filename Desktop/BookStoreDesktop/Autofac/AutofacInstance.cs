751884850a9a6f2d21e55d383f0681bcae340d91
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-04-02 15:35:13 +0700

    Using DI with Autofac in Winform

---
+++ Desktop/BookStoreDesktop/Autofac/AutofacInstance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using BookStoreDesktop.Autofac;
+namespace BookStoreDesktop.Autofac
+{
+    public class AutofacInstance
+    {
+        public static T GetInstance<T>()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new AutofacModule());
+            IContainer Container = builder.Build();
+            using(var scope = Container.BeginLifetimeScope())
+            {
+                return scope.Resolve<T>();
+            }
+        }
+    }
+}
