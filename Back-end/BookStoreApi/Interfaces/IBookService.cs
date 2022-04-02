7b3cd72281d37be948a341268b3afa61b655c8ed
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-04-02 15:06:58 +0700

    Service interface in BookStoreAPI

---
+++ Back-end/BookStoreApi/Interfaces/IBookService.cs
@@ -0,0 +1,14 @@
+﻿using BookStoreApi.Models;
+namespace BookStoreApi.Interfaces
+{
+    public interface IBookService
+    {
+        Task<IEnumerable<Book>> GetAsync();
+        Task<Book?> GetAsync(string id);
+        Task CreateAsync(Book newBook);
+        Task UpdateAsync(string id, Book updateBook);
+        Task DeleteAsync(string id);
+        Task<IEnumerable<Book>> ValidateBook(string id, string name);
+        Task<IEnumerable<Book>> ListBookByCategoryId(string id);
+    }
+}
