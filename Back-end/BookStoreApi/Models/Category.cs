1a392e9389cb4cbf1da62215261d739cbba6456c
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-04-02 13:45:31 +0700

    Models using Entity Framework Core

---
+++ Back-end/BookStoreApi/Models/Category.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApi.Models
+{
+    [Table("Categories")]
+    [Index(nameof(Category.Name), IsUnique = true)]
+    public class Category
+    {
+        [Key]
+        [StringLength(5)]
+        public string? Id { get; set; } = RandomID.RandomString(5);
+        [MaxLength(255),Unicode(true)]
+        public string Name { get; set; }
+        public int Quantity { get; set; }= 0;
+        private DateTime timeCreate = DateTime.UtcNow;
+        [Column("Create_at")]
+        public DateTime TimeCreate
+        {
+            get
+            {
+                TimeZoneInfo cstZone = TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time");
+                return TimeZoneInfo.ConvertTimeFromUtc(timeCreate, cstZone);
+            }
+            set { this.timeCreate = DateTime.UtcNow; }
+        }
+        public List<Book>? Book { get; set; }
+    }
+    public class CategoryShow
+    {
+        public string Id { get; set; }
+        public string CategoryName { get; set; }
+    }
+    public class CategoryDTO
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ Back-end/BookStoreApi/Models/Role.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApi.Models
+{
+    [Table("Roles")]
+    [Index(nameof(Role.Name),IsUnique = true)]
+    public class Role
+    {
+        [Key]
+        [StringLength(5)]
+        public string Id { get; set; } = RandomID.RandomString(5);
+        [StringLength(255), Unicode(true)]
+        public string Name { get; set; }
+        private DateTime timeCreate = DateTime.UtcNow;
+        [Column("Create_at")]
+        public DateTime TimeCreate
+        {
+            get
+            {
+                TimeZoneInfo cstZone = TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time");
+                return TimeZoneInfo.ConvertTimeFromUtc(timeCreate, cstZone);
+            }
+            set { this.timeCreate = DateTime.UtcNow; }
+        }
+        public int Quantity { get; set; }
+        public List<User>? User { get; set; }
+    }
+    public class RoleShow
+    {
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+    public class RoleDTO
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
