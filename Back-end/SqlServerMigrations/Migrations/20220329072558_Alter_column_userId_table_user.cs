4fa525dd91227adb5554569f9a03ff887400808c
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-04-02 15:34:22 +0700

    MigrationSQLServer if using SQLServer for BookStoreAPI

---
+++ Back-end/SqlServerMigrations/Migrations/20220329072558_Alter_column_userId_table_user.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SqlServerMigrations.Migrations
+{
+    public partial class Alter_column_userId_table_user : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Users",
+                type: "nvarchar(8)",
+                maxLength: 8,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(5)",
+                oldMaxLength: 5);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Users",
+                type: "nvarchar(5)",
+                maxLength: 5,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(8)",
+                oldMaxLength: 8);
+        }
+    }
+}
---
+++ Back-end/SqlServerMigrations/Migrations/20220329105907_Create_table_bills.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SqlServerMigrations.Migrations
+{
+    public partial class Create_table_bills : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bills",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: false),
+                    Value = table.Column<int>(type: "int", nullable: false),
+                    Create_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Currency = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bills", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bills");
+        }
+    }
+}
