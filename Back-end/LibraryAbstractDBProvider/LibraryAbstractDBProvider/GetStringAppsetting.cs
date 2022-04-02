6c2e281dce48e012faf2f1facd3068fcd9514407
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-04-02 14:12:19 +0700

    Get database default and connectionString

---
+++ Back-end/LibraryAbstractDBProvider/LibraryAbstractDBProvider/GetStringAppsetting.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryAbstractDBProvider
+{
+    public static class GetStringAppsetting
+    {
+        public static string DatabaseDefault()
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var databaseDefault = config.GetSection("DatabaseDefault").Value;
+            return databaseDefault;
+        }
+        public static IConfigurationRoot ConnectString()
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            return config;
+        }
+    }
+}
