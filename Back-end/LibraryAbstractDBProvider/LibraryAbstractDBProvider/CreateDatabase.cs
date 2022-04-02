b1500bbb95db4a935848fc5b62ec58ad37c2b480
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-04-02 14:11:10 +0700

    Create Database Provider (SQLServer - PostgreSQL)

---
+++ Back-end/LibraryAbstractDBProvider/LibraryAbstractDBProvider/CreateDatabase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryAbstractDBProvider
+{
+    public class CreateDatabase
+    {
+        public IDatabaseProvider GetDBProvider()
+        {
+            Type T = Type.GetType($"LibraryAbstractDBProvider.DBProvider.{GetStringAppsetting.DatabaseDefault()}");
+            if(T is null)
+            {
+                throw new NotImplementedException("Default database not a valid database type.");
+            }
+            var DBProvider = Activator.CreateInstance(T) as IDatabaseProvider;
+            return DBProvider;
+        }
+    }
+}
