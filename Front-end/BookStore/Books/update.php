3c025d31e5d9059367ce12f1d5dceb0bdbb11a84
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-04-02 14:32:26 +0700

    CURL Book

---
+++ Front-end/BookStore/Books/update.php
@@ -0,0 +1,24 @@
+<?php
+    include __DIR__ . '/../CallAPI.php';
+    if(isset($_POST["update"])){
+        $request = "book/detail/".$_POST["id"];
+        $data_array = array(
+            "Id" => $_POST["id"],
+            "Name" => $_POST["name"],
+            "Author" => $_POST["author"],
+            "CategoryId" => $_POST["categoryId"],
+            "Quantity" => $_POST["quantity"],
+            "Price" => $_POST["price"]
+        );
+        if(!empty($_FILES["file"]["name"])){
+            $data_array["File"] = curl_file_create($_FILES["file"]["tmp_name"],$_FILES["file"]["type"],$_FILES["file"]["name"]);
+        }
+        $header = array("Content-Type" => "application/json");
+        $result = CallAPI($request,"PUT",$data_array,$header);
+        if(isset($result["Error"]) || isset($result["errors"])){
+            print_r($result);
+        }else{
+            echo '<script>location.href = "index.php";</script>';
+        }
+    }
+?>
