c0c3edf95883e169f30c89e665d35e786e638987
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-04-02 15:27:49 +0700

    Config PostgreSQL

---
+++ Back-end/LibraryAbstractDBProvider/LibraryAbstractDBProvider/DBProvider/PostgreSQL.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryAbstractDBProvider.DBProvider
+{
+    public class PostgreSQL : AbstractDBProvider
+    {
+        public override void ConnectedDatabase(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(ConnectionString(), x => x.MigrationsAssembly("PostgreSQLMigrations"));
+        }
+
+        public override string ConnectionString()
+        {
+            IConfiguration config = GetStringAppsetting.ConnectString();
+            return config.GetConnectionString("PostgreSQL");
+        }
+    }
+}
