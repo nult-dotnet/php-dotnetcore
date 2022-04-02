989e7b12a9ff6061aea7d1eb443d50eeb754a8ab
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-04-02 14:39:27 +0700

    Delete Front-end directory

--- Front-end/BookStore/vendor/library.php
+++
@@ -1,6 +0,0 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-<link rel="stylesheet" href="/../../../php-dotnetcore/Front-end/BookStore/vendor/bootstrap/css/bootstrap.css">
-<link rel="stylesheet" href="/../../../php-dotnetcore/Front-end/BookStore/vendor/responsive.css">
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
