e85df7d790558a69a19efcffe0877ab834173baa
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-04-02 15:15:02 +0700

    UnitOfWork interface

---
+++ Back-end/BookStoreApi/RepositoryPattern/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using BookStoreApi.Models;
+using LibraryAbstractDBProvider;
+namespace BookStoreApi.RepositoryPattern
+{
+    public interface IUnitOfWork<TEntity> where TEntity : class
+    {
+        public IGenericRepository<TEntity> Repository { get; }
+    }
+}
