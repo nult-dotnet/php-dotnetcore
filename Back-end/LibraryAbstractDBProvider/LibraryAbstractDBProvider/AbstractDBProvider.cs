0c4d0b442a399569380576ce3b68fb78d1a37d4b
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-04-02 14:10:26 +0700

    Abstract database provider (SQLServer - PostgreSQL)

---
+++ Back-end/LibraryAbstractDBProvider/LibraryAbstractDBProvider/AbstractDBProvider.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryAbstractDBProvider
+{
+    public abstract class AbstractDBProvider : IDatabaseProvider
+    {
+        public abstract void ConnectedDatabase(DbContextOptionsBuilder optionsBuilder);
+
+        public abstract string ConnectionString();
+    }
+}
