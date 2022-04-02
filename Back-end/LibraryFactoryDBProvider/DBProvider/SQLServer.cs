0f257f5010b8dfc689e87fa6a8afaa6f6c229c36
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-04-02 14:13:53 +0700

    Library Factory Database provider

---
+++ Back-end/LibraryFactoryDBProvider/DBProvider/SQLServer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryFactoryDBProvider.DBProvider
+{
+    public class SQLServer : IDatabaseProvider
+    {
+        public void ConnectedDatabase(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString(), x => x.MigrationsAssembly("SqlServerMigrations"));
+        }
+        public string ConnectionString()
+        {
+            IConfiguration config = GetStringAppsetting.ConnectString();
+            return config.GetConnectionString("SQLServer");
+        }
+    }
+}
