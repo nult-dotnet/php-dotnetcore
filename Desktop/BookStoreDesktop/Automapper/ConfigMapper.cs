d19a2ebde0d84b9f42604a8a702ffd7799966b58
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-04-02 15:35:37 +0700

    Register Automapper

---
+++ Desktop/BookStoreDesktop/Automapper/ConfigMapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStoreDesktop.Automapper;
+namespace BookStoreDesktop.Automapper
+{
+    public static class ConfigMapper
+    {
+        public static IMapper configMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MapperBookStoreDesktop());
+            });
+            return config.CreateMapper();
+        }
+    }
+}
