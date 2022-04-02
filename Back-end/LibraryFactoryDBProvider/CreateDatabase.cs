1593ff869d32b9c6d6e9440639b1fa620440ac64
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-04-02 15:32:50 +0700

    Library Factory Db Provider

---
+++ Back-end/LibraryFactoryDBProvider/CreateDatabase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryFactoryDBProvider
+{
+    public static class CreateDatabase
+    {
+        public static IDatabaseProvider GetDBProvider()
+        {
+            Type T = Type.GetType($"LibraryFactoryDBProvider.DBProvider.{GetStringAppsetting.DatabaseDefault()}");
+            if(T is null)
+            {
+               throw new NotImplementedException("Default database not a vaild database type");
+            }
+            var DBProvider = Activator.CreateInstance(T) as IDatabaseProvider;
+            return DBProvider;
+        }
+    }
+}
