c1108c3fe93f8c269c6e997863d001b8848939e4
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-04-02 14:33:51 +0700

    Layout (Sidebar - Header - Footer - CSS)

---
+++ Front-end/BookStore/layouts/partials/header.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="/../../php-dotnetcore/Front-end/BookStore/layouts/css/style_header.css">
+<div class="header_container">
+    <!-- <ul class="header_item">
+        <li><label class="header_item--child" id="hide_slidebar" for="check_hide_slidebar"><i style="font-size: 20px;" class="fas fa-bars"></i></label></li>
+        <input type="checkbox" id="check_hide_slidebar" hidden>
+        <li class="header_item--child"><a href="/../Amazing-PHP/backend/">Trang chủ</a></li>
+        <li class="header_item--child"><a href="/../Amazing-PHP/backend/products/">Sản phẩm</a></li>
+        <li class="header_item--child"><a href="/../Amazing-PHP/backend/order/">Đơn hàng</a></li>
+        <li class="header_item--child"><a href="/../Amazing-PHP/frontend/">Giao diện FrontEnd</a></li>
+    </ul>
+    <ul class="header_item">
+        <li class="header_item--child"><i class="fas fa-search"></i></li>
+        <li class="header_item--child"><i class="far fa-comments"></i></li>
+        <li class="header_item--child"><i class="fas fa-bell"></i></li>
+    </ul> -->
+</div>
+<?php date_default_timezone_set("Asia/Ho_Chi_Minh");?>
