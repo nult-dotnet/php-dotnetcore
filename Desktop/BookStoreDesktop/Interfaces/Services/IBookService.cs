82d5a0facde9d67b8150ff7b938a159cdf633b9d
Công Thiện <91828383+congthienn@users.noreply.github.com>
2022-04-02 15:38:02 +0700

    Service interface

---
+++ Desktop/BookStoreDesktop/Interfaces/Services/IBookService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStoreDesktop.Models;
+namespace BookStoreDesktop.Interfaces.Services
+{
+    public interface IBookService
+    {
+        List<Book> GetAllBook();
+        Book GetBookById(string id);
+        bool CreateBook(BookDTO book);
+        bool UpdateBook(BookDTO book, string id);
+        bool DeleteBook(string id);
+        List<Book> GetBookByName(string name);
+        bool CheckCategoryId(int categoryId);
+    }
+}
